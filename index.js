a29a52c10f343c35e80de5c43a68e172925a5e0a
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-02-06 23:22:48 +0530

    using nodemon

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World22!')
+    res.send('Hello World!')
+  })
+  app.get('/about', (req, res) => {
+    res.send('About us!')
+  })
+  app.get('/contact', (req, res) => {
+    res.send('contact us')
+  })
+  app.get('/blog', (req, res) => {
+    res.send('Hello blog!')
   })
 
 app.listen(port, () =>{
